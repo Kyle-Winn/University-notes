d0aed98cbcb4f233ce87dee1cf3a041d660de333
fast-fingers <kylealexanderwinn@gmail.com>
2022-11-04 18:14:23 +0000

    doiwj

--- FP-Tutorial7/Tutorial7.hs
+++ FP-Tutorial7/Tutorial7.hs
@@ -16,12 +16,25 @@ polygon dist turns = copy turns (Go dist :#: Turn (360/(fromIntegral turns)))
 
 -- 2. snowflake
 snowflake :: Int -> Command
-snowflake x = undefined
+snowflake x = f x :#: n :#: n :#: f x :#: n :#: n :#: f x :#: n :#: n 
+    where
+    f 0 = Go 10
+    f x = f (x-1) :#: p :#: f (x-1) :#: n :#: n :#: f (x-1) :#: p :#: f (x-1) 
+    n   = Turn (-60)
+    p   = Turn (60)
 
 -- 3. sierpinski
+--Using different colours for f and g as the example did.
 sierpinski :: Int -> Command
-sierpinski =  undefined
-     
+sierpinski x =  f x
+    where 
+    f 0 = GrabPen red :#: Go 10
+    f x = g (x-1) :#: p :#: f (x-1) :#: p :#: g (x-1)
+    g 0 = GrabPen blue :#: Go 10
+    g x = f (x-1) :#: n :#: g (x-1) :#: n :#: f (x-1)
+    n   = Turn (-60)
+    p   = Turn (60)
+
 -- 4. hilbert
 hilbert :: Int -> Command
 hilbert =  undefined
@@ -34,23 +47,52 @@ dragon =  undefined
 
 -- 6a. split
 split :: Command -> [Command]
-split =  undefined
+split (p :#: q) = (split p) ++ (split q)
+split (Sit) = []
+split (p) = [p]
 
 -- 6b. join
 join :: [Command] -> Command
-join =  undefined
+join []         = Sit
+join (cmd:cmds) = cmd :#: join cmds
+
+--Alternatively
+
+joinFoldr :: [Command] -> Command
+joinFoldr cmds = foldr (:#:) Sit cmds
+
+prop_bothJoins :: [Command] -> Bool
+prop_bothJoins cmds = joinFoldr cmds == join cmds
+-- +++ OK, passed 100 tests.
+
 
 -- 6c. equivalent
 equivalent :: Command -> Command -> Bool
-equivalent =  undefined
+equivalent x y = split x == split y
 
 -- 6d. testing join and split
 prop_split_join :: Command -> Bool
-prop_split_join =  undefined
+prop_split_join c = equivalent c (join (split c))
+
+-- +++ OK, passed 100 tests.
 
 prop_split :: Command -> Bool
-prop_split =  undefined
+prop_split cmd = and[ (x /= Sit) && (length(split(x)) == 1)| x <- split cmd]
+
 
 -- 7. optimise
+
+--q contains no Sit, Go 0 or Turn 0 commands, unless the command is equivalent to Sit.
+--q contains no adjacent Go commands.
+--q contains no adjacent Turn commands.
+
+sitlessJoin :: [Command] -> Command
+sitlessJoin cmd = foldr1 (:#:) cmd
+
 optimise :: Command -> Command
-optimise =  undefined
+optimise cmd = sitlessJoin  (handleGoandTurn (filter (/= Turn 0) (handleGoandTurn ( filter (/= Go 0) (split cmd)))))
+    where 
+        handleGoandTurn [] = []
+        handleGoandTurn (Go x : Go y : xs)     = handleGoandTurn (Go (x+y) : xs)
+        handleGoandTurn (Turn x : Turn y : xs) = handleGoandTurn (Turn (x+y) : xs)
+        handleGoandTurn (x:xs)                 = x : handleGoandTurn xs
